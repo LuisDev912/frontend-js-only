b74ce382e69e6f484c8ae25a2fde510fe6c14030
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-07 15:01:07 -0300

    feat: Add justifyContent and alignItems functions

--- src/flexbox.js
+++ src/flexbox.js
@@ -6,10 +6,23 @@ export class Flexbox {
         this.element.style.gap = "0.5rem";
     }
 
-    //justifyContent, alignItems, ...
+    justifyContent(value) {
+        this.element.style.justifyContent = value;
+        return this;
+    }
+
+    alignItems(value) {
+        this.element.style.alignItems = value;
+        return this;
+    }
 
     append(child) {
         this.element.appendChild(child);
         return this; 
     }
+
+    render(parent = document.body) {
+        parent.appendChild(this.element);
+    }
 }
+
