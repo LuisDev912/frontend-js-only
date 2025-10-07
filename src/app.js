bdfe36c3f93b2f129f801de63f70901a0d91c105
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-07 15:12:40 -0300

    "feat: Add functions in components.js - Create createButton(), createInput() and createTitle() functions. - Import flexbox.js and components.js functions to app.js"

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import { Flexbox } from "./flexbox";
+import { createButton, createInput, createTitle } from "./components";
+
+// const layout = new Flexbox('column')
+//     .alignItems("center")
+//     .justifyContent("center");
+
+// layout.append(createTitle("Mini ToDo List (JS Only"));
---
+++ src/components.js
@@ -0,0 +1,11 @@
+export function createTitle(text) {
+    // code
+}
+
+export function createButton(text, onClick) {
+    //code
+}
+
+export function createInput(placeholder) {
+    //code
+}
