71649902b35c5691b9d8aefe5e21fd21cdc41148
Luis Calleja <luiscallejaenrique@gmail.com>
2025-10-08 13:24:12 -0300

    feat: Finish functions in component.js

--- src/components.js
+++ src/components.js
@@ -1,11 +1,23 @@
 export function createTitle(text) {
-    // code
+    const h1 = document.createElement('h1');
+    h1.innerText = text;
+    return h1;
 }
 
 export function createButton(text, onClick) {
-    //code
+    const btn = document.createElement('button');
+    btn.innerText = text;
+    btn.addEventListener('click', onClick);
+    btn.style.padding = "0.5rem, 1rem";
+    btn.style.borderRadius = "5px";
+    return btn;
 }
 
 export function createInput(placeholder) {
-    //code
+    const input = document.createElement('input');
+    input.style.placeholder = placeholder;
+    input.style.borderRadius = "5px";
+    input.style.border = "1px solid #ccc";
+    input.style.padding = "0.5rem";
+    return input;
 }
